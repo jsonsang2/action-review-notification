44d8ef5fa5773bfa7c8983c368cea830b8c92868
Woosang Lee <woosanglee@Woosangs-MacBook-Pro.local>
2022-06-04 13:30:01 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import {Client} from './client'
 
 async function run(): Promise<void> {
   try {
-
     const status: string = core.getInput('status')
     const mention = core.getInput('mention')
     const author_name = core.getInput('author_name')
@@ -43,7 +42,9 @@ async function run(): Promise<void> {
       github_base_url,
       process.env.SLACK_WEBHOOK_URL
     )
-    await client.send(await client.prepare(custom_payload));
+
+    await client.send('hello')
+    //await client.send(await client.prepare(custom_payload));
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
